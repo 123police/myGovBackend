bab1d425e2617e9d7274bdefb47e70dd451a03ad
Hakym <40834956+hakymz@users.noreply.github.com>
2024-06-09 00:18:04 +0100

    first commit

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const path = require("path");
 
-// Database connection
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// data base connection
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DataBase connected"))
   .catch((err) => console.log("DataBase not connected", err));
 
-// Middleware
+//   middle ware
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-
-// Serve static files from the "public" directory
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use("/", require("./routes/authRoutes"));
 
 const port = 3001;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
