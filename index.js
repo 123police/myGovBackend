168d3c5aa8230bdebbe26c61cd7a47284b75682b
123police <cameronbush2023@gmail.com>
2024-06-17 11:42:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.use(express.static("public"));
 
 app.use("/", require("./routes/authRoutes"));
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "NOT_FOUND",
+    message: "The requested resource was not found"
+  });
+});
+
 const port = 3001;
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
