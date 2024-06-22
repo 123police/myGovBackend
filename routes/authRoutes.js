62f94642a5a9e3037474a25029c4176cbce2fe74
123police <cameronbush2023@gmail.com>
2024-06-22 08:04:44 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,32 +1,10 @@
-//
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
-const {
-  test,
-  login,
-  forgotPassword,
-  getAllUsers,
-} = require("../controllers/authController");
+const { login, forgotPassword, getAllUsers } = require("../controllers/authController");
 
-// MIDDLEWARE
-
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use("/api", require("./routes/authRoutes"));
-
-// Apply CORS middleware to the entire app
-app.use(cors(corsOptions));
-
+// Define routes
 router.post("/login", login);
-
 router.post("/forgotPassword", forgotPassword);
-
 router.get("/admin/users", getAllUsers);
 
 module.exports = router;
